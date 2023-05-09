bc8451f559e44f35eae7a30a2de339dae7ff263a
mpzinke <mathew.zinke@gmail.com>
2023-05-08 19:03:26 -0500

    Adds virtual curtain

--- Backend/Server/Root/api/v1_0/curtains/curtain_id/events/new/__init__.py
+++ Backend/Server/Root/api/v1_0/curtains/curtain_id/events/new/__init__.py
@@ -14,25 +14,25 @@ __author__ = "MPZinke"
 ########################################################################################################################
 
 
-from datetime import datetime;
-from flask import request;
-import json;
+from datetime import datetime
+from flask import request
+import json
 
 
-from SmartCurtain import SmartCurtain;
+from SmartCurtain import SmartCurtain
 
 
 # `POST /api/v1.0/curtain/<int:curtain_id>/events/new`
 # Creates a new curtain's event with the JSON body.
 def POST(smart_curtain: SmartCurtain, curtain_id: int):
-	if((curtain := smart_curtain.Curtain(id=curtain_id)) is None):
-		raise Exception("Not found");  #TODO
+	if((curtain := smart_curtain["-"]["-"][curtain_id]) is None):
+		raise Exception("Not found")  #TODO
 
-	body: dict = request.json;
+	body: dict = request.json
 	if((percentage := body.get("percentage")) is None):
-		raise Exception("Bad body");  #TODO
-	kwargs = {"Options_id": body.get("Options.id")};
-	kwargs["time"] = datetime.strptime("%Y-%m-%d %H:%M:%S", body["time"]) if("time" in body) else None;
+		raise Exception("Bad body")  #TODO
+	kwargs = {"Options_id": body.get("Options.id")}
+	kwargs["time"] = datetime.strptime("%Y-%m-%d %H:%M:%S", body["time"]) if("time" in body) else None
 
-	event_id = curtain._new_event(percentage, **kwargs);
-	return json.dumps(dict(curtain.CurtainEvent(id=event_id)), default=str);
+	new_event = curtain.new_CurtainEvent(percentage=percentage)  #TODO: Finish
+	return dict(new_event)
